abcdb20716cdc81843a8db8da8f3af4e4639200e
Eric Fluckiger <ericfluckiger@yahoo.com>
2022-08-08 22:24:43 -0600

    added stuff

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,41 @@
-const addition = (a: number, b: number) => {
-  return a + b;
-};
+import dotenv from 'dotenv';
+import * as path from 'path';
+import * as fs from 'fs';
 
-const number1 = 5;
-const number2 = 10;
-const result = addition(number1, number2);
+import { GoogleDriveService } from './googleDriveService';
 
-console.log('The result is %d', result);
+dotenv.config();
+
+const driveClientId = '417781912750-ss7tp9ofi6k2s40cmpgcr0q7k5eu7pj1.apps.googleusercontent.com';
+const driveClientSecret = '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';
+const driveRedirectUri = 'https://developers.google.com/oauthplayground';
+const driveRefreshToken = '1//04UA0UfKuTbcrCgYIARAAGAQSNwF-L9IrPGmunEFxuN2d0NHJs6RwRY3-iHLIHtZGtli75JTMLNmawaZ21daAJlDkLe7lrks7dno';
+
+(async () => {
+  const googleDriveService = new GoogleDriveService(driveClientId, driveClientSecret, driveRedirectUri, driveRefreshToken);
+
+  const finalPath = path.resolve(__dirname, '../public/img.jpg');
+  const folderName = 'Tool Data';
+
+  if (!fs.existsSync(finalPath)) {
+    throw new Error('File not found!');
+  }
+
+  let folder = await googleDriveService.searchFolder(folderName).catch((error) => {
+    console.error(error);
+    return null;
+  });
+
+  if (!folder) {
+    folder = await googleDriveService.createFolder(folderName);
+  }
+
+  await googleDriveService.saveFile('SpaceX', finalPath, 'image/jpg', folder.id).catch((error) => {
+    console.error(error);
+  });
+
+  console.info('File uploaded successfully!');
+
+  // Delete the file on the server
+  fs.unlinkSync(finalPath);
+})();
